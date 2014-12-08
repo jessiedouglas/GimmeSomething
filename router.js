66aaf242570b63c9317ba02c1b5b9cbd4011890a
App Academy Student <appacademy@Apps-Mac-mini-5.local>
2014-12-08 12:21:52 -0500

    Add more options

--- router.js
+++ router.js
@@ -3,27 +3,35 @@
     this.$el = $(el);
     this.routes = {};
 
-    this.route("/pizza", "pizza", "bff");
-    this.route("/tacos", "tacos", "wtf");
+    this.route("/pizza", "pizza");
+    this.route("/tacos", "mexican");
+    this.route("/sushi", "sushi");
+    this.route("/pad_thai", "thai");
+    this.route("/curry", "indian");
+    this.route("/burgers", "burgers");
+    this.route("/spaghetti", "italian");
+    this.route("/waffles", "diner");
+    this.route("/cookies", "bakery");
+    this.route("/ice_cream", "ice cream");
+    this.route("/beer", "bar");
 
     window.addEventListener("hashchange", this.switchPage.bind(this));
     window.addEventListener("load", this.switchPage.bind(this));
   };
 
-  Router.prototype.route = function (path, templateId, controller) {
+  Router.prototype.route = function (path, searchTerm) {
     this.routes[path] = {
-      templateId: templateId,
-      controller: controller
+      searchTerm: searchTerm
     };
   };
 
   Router.prototype.switchPage = function () {
     $main = $("#main");
-    var url = location.hash.slice(1) || "/";
+    var url = location.hash.slice(1) || "/pizza";
 
     var route = this.routes[url] || {};
 
-    if ($main && route.controller) {
+    if ($main && route.searchTerm) {
       var $new = $("#" + url.slice(1));
       $main.html($new.html());
     }
