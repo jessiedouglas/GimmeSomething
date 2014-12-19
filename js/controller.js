dbf99179d1034f29895569176d167a8aca66dc7c
Florin Petre <fmihailpetre@gmail.com>
2014-12-19 16:28:16 -0500

    Fix async issues

--- js/controller.js
+++ js/controller.js
@@ -115,7 +115,7 @@
       var node = document.getElementById("map_results");
       var service = new google.maps.places.PlacesService(node);
       service.nearbySearch(request, this.parseSearch.bind(this));
-    }, 0);
+    }.bind(this), 0);
   };
 
   GimmeSomething.Controller.prototype.parseSearch = function (resp, status) {
